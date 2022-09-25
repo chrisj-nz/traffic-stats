05a58254b4546bf56bdbea8e9f4d49afd8498caf
Chris Jones <chrisj.nz@gmail.com>
2022-09-26 02:21:58 +1300

    vsdv

--- visitor-stats.js
+++ visitor-stats.js
@@ -3,8 +3,8 @@ import fs from 'fs';
 import { updateCsvWithLatestViews } from './csv-aggregator.js';
 
 const csvFilePath = process.env.CSV_FILEPATH;
+const repository = process.env.GITHUB_REPOSITORY;
 const gitHubToken = process.env.GITHUB_TOKEN;
-const repository = process.env.REPOSITORY;
 const getViews = github.getOctokit(gitHubToken).rest.repos.getViews;
 
 updateCsvWithLatestViews({fs, getViews, csvFilePath, repository});
