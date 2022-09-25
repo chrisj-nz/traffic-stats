669a04278607b517f10c58504ca29c070afd0468
Chris Jones <chrisj.nz@gmail.com>
2022-09-25 13:05:10 +1300

    sdsd

--- traffic-stats.js
+++ traffic-stats.js
@@ -6,6 +6,7 @@ const csvFilePath = process.env.CSV_FILEPATH;
 const gitHubToken = process.env.GITHUB_TOKEN;
 const repository_owner = process.env.REPOSITORY_OWNER;
 const repository_name = process.env.REPOSITORY_NAME;
+console.log(`repository_name: ${repository_name}`);
 const repos = github.getOctokit(gitHubToken).rest.repos;
 
 createCsv({fs, repos, csvFilePath, repository_owner, repository_name});
